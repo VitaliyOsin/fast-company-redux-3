8d4d0b114500df4895814c609037cafe7e38bfaa
Vitaliy Osin <d_dva@list.ru>
2022-03-02 14:58:56 +0700

    first note

---
+++ src/app/components/common/protectedRoute.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { useSelector } from "react-redux";
+import { getIsLoggedIn } from "../../store/users";
+
+const ProtectedRoute = ({ component: Component, children, ...rest }) => {
+    const isLoggedIn = useSelector(getIsLoggedIn());
+    return (
+        <Route { ...rest } render={ (props) => {
+            if (!isLoggedIn) {
+                return <Redirect to={{
+                    pathname: "/login",
+                    state: {
+                        from: props.location
+                    }
+                }} />;
+            }
+            return Component ? <Component {...props} /> : children;
+        }}/>
+    );
+};
+
+ProtectedRoute.propTypes = {
+    component: PropTypes.func,
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+    location: PropTypes.object
+};
+
+export default ProtectedRoute;
---
+++ src/app/store/createStore.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import professionsReducer from "./professions";
+import qualitiesReducer from "./qualities";
+import usersReducer from "./users";
+
+const { combineReducers } = require("redux");
+
+const rootReducer = combineReducers({
+    qualities: qualitiesReducer,
+    professions: professionsReducer,
+    users: usersReducer
+});
+
+export function createStore() {
+    return configureStore({
+        reducer: rootReducer
+    });
+}
